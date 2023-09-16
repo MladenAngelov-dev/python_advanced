2b9ebac0eb62a9abfdf0d663511acb29104d859f
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-09-16 12:04:07 +0300

    first and second

---
+++ stacks_queues_exer/stacked_queries.py
@@ -0,0 +1,20 @@
+n = int(input())
+my_stack = []
+
+for _ in range(n):
+    command = input().split()
+    if command[0] == "1":
+        number = int(command[1])
+        my_stack.append(number)
+    elif my_stack:
+        if command[0] == "2":
+            my_stack.pop()
+        elif command[0] == "3":
+            print(max(my_stack))
+        elif command[0] == "4":
+            print(min(my_stack))
+
+while my_stack:
+    print(my_stack.pop(), end="")
+    if my_stack:
+        print(", ", end="")
